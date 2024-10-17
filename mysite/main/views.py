27aab2e7ba33224e1478672ed135608fcfbb66c0
Samuel Craft <craftsa@bc.edu>
2024-10-17 19:26:05 -0400

    Update views.py
    Added (very basic) view functions for the 5 pages of our web application

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("JOAKS")
-
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, 'home.html', {})
+
+
+def actions_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, 'actions.html', {})
+
+
+def rewards_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, 'rewards.html', {})
+
+
+def profile_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, 'profile.html', {})
+
+
+def campaigns_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, 'campaigns.html', {})
