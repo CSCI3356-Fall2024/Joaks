ded25949467b0b4d15874d3b9d0f183c1f7f6bde
Samuel Craft <craftsa@bc.edu>
2024-10-17 19:30:19 -0400

    Update urls.py

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,7 +1,12 @@
+from django.contrib import admin
 from django.urls import path
-
-from . import views
+from views import home_view, actions_view, rewards_view, profile_view, campaigns_view
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('actions/', actions_view, name='actions'),
+    path('', home_view, name='home'),
+    path('rewards/', rewards_view, name='rewards'),
+    path('profile/', profile_view, name='profile'),
+    path('campaigns/', campaigns_view, name='campaigns')
 ]
