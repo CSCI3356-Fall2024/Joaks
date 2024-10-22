19f3229f570df7d71b6ffd58319bb5429c544dbf
Julia Lin <linjulia12@gmail.com>
2024-10-21 21:47:23 -0400

    added home page for guest

--- mysite/googleauth/views.py
+++ mysite/googleauth/views.py
@@ -7,11 +7,18 @@ def login(request):
     return render(request, 'login.html')
 
 # Home view (requires login)
-@login_required
+#@login_required
+#def home(request):
+#    return render(request, 'home.html')
+
+# Home view for both logged-in and logged-out users
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        return render(request, 'home_logged_in.html')  # Template for logged-in users
+    else:
+        return render(request, 'home_logged_out.html')  # Template for logged-out users
 
 # Logout view
 def logout_view(request):
     logout(request)
-    return redirect('login')  # Redirect to login page or any other page
+    return redirect('home')  # Redirect to home page or any other page
