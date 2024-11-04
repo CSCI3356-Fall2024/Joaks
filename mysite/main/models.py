9852a67881b58a6833b6868f86d3ecd3c01ceef8
samcraftt <craftsa@bc.edu>
2024-11-03 20:13:53 -0500

    Save local changes before pulling

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .majors import MAJORS
 
 # Create your models here.
 
@@ -11,10 +12,10 @@ class CustomUser(AbstractUser):
     role = models.CharField(max_length=20, choices=CHOICES, default='student')
     class_year = models.CharField(max_length=50, blank=True)
     school = models.CharField(max_length=50, blank=True)
-    major = models.CharField(max_length=50, blank=True)
-    double_major = models.CharField(max_length=50, blank=True)
-    minor = models.CharField(max_length=50, blank=True)
-    double_minor = models.CharField(max_length=50, blank=True)
+    major = models.CharField(max_length=50, choices=MAJORS, blank=True)
+    double_major = models.CharField(max_length=50, choices=MAJORS, blank=True)
+    minor = models.CharField(max_length=50, choices=MAJORS, blank=True)
+    double_minor = models.CharField(max_length=50, choices=MAJORS, blank=True)
     referral = models.CharField(max_length=50, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     finished_profile = models.BooleanField(default=False)
