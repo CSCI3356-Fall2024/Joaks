a2598bc6f8c099f3dfc576532e21d6f907fe67fe
Julia Lin <linjulia12@gmail.com>
2024-11-06 11:15:54 -0500

    regisstered admin

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Campaign
+from .models import CustomUser, Campaign, UpcomingEvents, Milestone
 # Register your models here.
 
 @admin.register(CustomUser)
@@ -10,4 +10,16 @@ class CustomUser(admin.ModelAdmin):
 @admin.register(Campaign)
 class CampaignAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'start_date', 'end_date', 'locations', 'points', 'show_news', 'integration_method')  
+
+
+@admin.register(Milestone)
+class MilestoneAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_by', 'show_news')  # Adjust fields as needed
+
+@admin.register(UpcomingEvents)
+class UpcomingEventsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'description', 'start_date', 'end_date', 
+        'locations', 'show_news', 'integration_method', 'created_by'
+    )  # Specify fields to display in the admin list view
 
