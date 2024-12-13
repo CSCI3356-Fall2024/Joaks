15f4c26136544f4c9b1531a46afbe963b12706fe
AndrewKallmeyer1 <90013113+AndrewKallmeyer1@users.noreply.github.com>
2024-12-13 16:34:03 -0500

    deployment steps

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-v-xh5vx-g1r$jx0_l31o5^#7h^o03pi0lkg5jvq)1r0*zb$-r*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
