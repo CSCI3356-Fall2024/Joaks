72f419dedd126b965dcfb970e3ecc289b86bb00d
AndrewKallmeyer1 <90013113+AndrewKallmeyer1@users.noreply.github.com>
2024-12-15 16:45:07 -0500

    WHY RENDER WHY

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -169,7 +169,6 @@ MEDIA_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_QUERYSTRING_AUTH = False
-AWS_DEFAULT_ACL = 'public-read'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #add css
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
